7636034b19fef911ae0f910fee61e18f47ad072b
Raúl Enrique González Bondarchuk <96430972+raulbondarchuk@users.noreply.github.com>
2025-09-18 08:40:41 +0200

    save

--- service/scripts/ps/help.ps1
+++ service/scripts/ps/help.ps1
@@ -11,7 +11,9 @@ param(
 $text = @"
 === $AppName - ayuda rapida de [make <cmd>] ===
 
-Flujo recomendado
+== RECOMENDACIONES ====================================================================
+
+Flujo de comandos (RECOMENDADO)
   1.0)   make all     - (init -> gen (deps + gen) -> wire -> run)
   1.1)   make init    - instalar herramientas
   1.2)   make deps    - refrescar buf.lock si hace falta, se puede omitir y utilizar make gen
@@ -20,7 +22,17 @@ Flujo recomendado
   2.0)   make build   - compilar binario ($Bin)
   3.0)   make run     - ejecutar con kratos (o 'make gorun')
 
-Comandos
+Workflow de modulos (RECOMENDADO)
+  make module name="foo" - crear modulo completo (proto, feature, repo, biz, service, wire) En caso de que ya exista, se crea una version nueva.
+  make module-delete name="foo" - eliminar modulo foo (todas versiones)
+  make module-delete name="foo" version="v2" - eliminar solo version v2 (puede ser cualquier versión existente)
+
+Commit + version automatica
+  make commit t="Titulo" d="Descripcion"   - commit con version automatica (etiqueta desde app.version en $ConfigPath)
+
+== COMANDOS ====================================================================
+
+Comandos principales
   make help     - esta ayuda
   make init     - instalar/actualizar generadores + go mod tidy
   make deps     - actualizar buf.lock si buf.yaml cambio
@@ -30,17 +42,23 @@ Comandos
   make run      - kratos run (hot reload)
   make gorun    - go run ./$CmdDir -conf ./configs
   make clean    - limpiar bin/, wire_gen.go, *.pb.go, swagger/openapi
-
-Commit + version automatica
-  make commit t="Titulo" d="Descripcion"   - commit con version automatica (etiqueta desde app.version en $ConfigPath)
+  make docs     - regenerar solo documentacion (docs/, docs/openapi)
   
+Comandos de modulo
+  make module-proto name="foo"           - generar solo proto
+  make module-feature name="foo"         - generar solo feature
+  make module-repo name="foo"            - generar solo repo
+  make module-biz name="foo"             - generar solo biz
+  make module-service name="foo"         - generar solo service
+  make module-wire name="foo"            - generar solo wire
 
+== CONFIG ====================================================================
+  
 Config (Makefile)
   CMD_DIR=$CmdDir
   BIN=$Bin
   BUF_GEN=$BufGen
   CONFIG_PATH=$ConfigPath
-  
 "@
 
 function Write-WithYellowMake {
@@ -66,7 +84,7 @@ function Write-WithYellowMake {
 
 # salida con color suave para encabezados
 foreach ($line in $text -split "`r?`n") {
-  if ($line -match '^(===|Flujo recomendado|Comandos|Commit \+ version automatica|Config)') {
+  if ($line -match '^(===|Flujo de comandos|Recomendaciones|Comandos principales|Comandos de modulo|Workflow de modulos|Commit \+ version automatica|Config|COMANDOS =+|RECOMENDACIONES =+|CONFIG =+)') {
     Write-Host $line -ForegroundColor Cyan
   } else {
     Write-WithYellowMake $line
